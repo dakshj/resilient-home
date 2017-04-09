499b8f5503dfdfe5b05e3c46a3fbe539ee825dfc
Nidhi Mundra <nidhi.mundra05@gmail.com>
2017-04-09 00:19:30 -0400

    Add LruCache

---
+++ app/src/main/java/com/resilienthome/util/LruCache.java
@@ -0,0 +1,18 @@
+package com.resilienthome.util;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class LruCache<K, V> extends LinkedHashMap<K, V> {
+    private int cacheSize;
+
+    public LruCache(int cacheSize) {
+        super(16, 0.75f, true);
+        this.cacheSize = cacheSize;
+    }
+
+    @Override
+    protected boolean removeEldestEntry(final Map.Entry<K, V> eldest) {
+        return size() >= cacheSize;
+    }
+}
