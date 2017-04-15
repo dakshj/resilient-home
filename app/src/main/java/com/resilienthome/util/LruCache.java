cfc1566a1783e223ec908674bd35ea642ddb0e98
Nidhi Mundra <nidhi.mundra05@gmail.com>
2017-04-15 13:27:44 -0400

    Fix LruCache to have store the given number of entries

--- app/src/main/java/com/resilienthome/util/LruCache.java
+++ app/src/main/java/com/resilienthome/util/LruCache.java
@@ -4,15 +4,16 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class LruCache<K, V> extends LinkedHashMap<K, V> {
+
     private int cacheSize;
 
-    public LruCache(int cacheSize) {
+    LruCache(int cacheSize) {
         super(16, 0.75f, true);
         this.cacheSize = cacheSize;
     }
 
     @Override
     protected boolean removeEldestEntry(final Map.Entry<K, V> eldest) {
-        return size() >= cacheSize;
+        return size() > cacheSize;
     }
 }
