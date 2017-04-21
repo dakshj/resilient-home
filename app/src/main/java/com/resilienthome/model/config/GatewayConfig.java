049bd6012407dcc1a5bc2584151b7688731f4061
Nidhi Mundra <nidhi.mundra05@gmail.com>
2017-04-21 16:42:06 -0400

    Add cacheSize to GatewayConfig model class

--- app/src/main/java/com/resilienthome/model/config/GatewayConfig.java
+++ app/src/main/java/com/resilienthome/model/config/GatewayConfig.java
@@ -8,7 +8,13 @@ public class GatewayConfig extends ServerConfig implements Serializable {
 
     private Address dbAddress;
 
+    private int cacheSize;
+
     public Address getDbAddress() {
         return dbAddress;
+    }
+
+    public int getCacheSize() {
+        return cacheSize;
     }
 }
